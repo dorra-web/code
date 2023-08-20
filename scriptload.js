4400364f70c8055915565930827d25fae28d8ce1
lihatdiskon <140128615+lihatdiskon@users.noreply.github.com>
2023-08-20 09:43:05 +0700

    Update scriptload.js

--- scriptload.js
+++ scriptload.js
@@ -1,7 +1,29 @@
-function loadScript(url, callback) {
-  var script = document.createElement("script");
-  script.type = "text/javascript";
-  script.src = url;
-  script.onload = callback;
-  document.body.appendChild(script);
-}
+$(window).on('load', function () {
+  setTimeout(function () {
+    $('.loader').fadeOut('slow');
+  }, 500);
+  $('.loader').remove('slow');
+});
+
+$(".bg-top").parent().addClass('b-top');
+$(".bg-bottom").parent().addClass('b-bottom');
+$(".bg-center").parent().addClass('b-center');
+$(".bg_size_content").parent().addClass('b_size_content');
+$(".lazyload").parent().addClass('bg-size');
+$(".lazyload.blur-up").parent().addClass('blur-up lazyload');
+
+jQuery('.lazyload').each(function () {
+
+  var el = $(this),
+    src = el.attr('src'),
+    parent = el.parent();
+
+  parent.css({
+    'background-image': 'url(' + src + ')',
+    'background-size': 'cover',
+    'background-position': 'center',
+    'display': 'block'
+  });
+
+  el.hide();
+});
